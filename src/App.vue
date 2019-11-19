5bc31fb7a380008393a00074ea4def07c6e99b9a
Lasse Krarup <latze95@gmail.com>
2019-11-19 23:57:56 +0100

    Basic skeleton of website with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app>
+        <NavigationDrawer :items="navigationLinks"></NavigationDrawer>
+        <v-content>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import NavigationDrawer from "@/components/NavigationDrawer.vue";
+
+export default Vue.extend({
+    name: "App",
+
+    components: {
+        NavigationDrawer
+    },
+
+    data: () => ({
+        navigationLinks: [
+            {
+                name: "Home",
+                path: "/",
+                icon: "home"
+            },
+            {
+                name: "About",
+                path: "/about",
+                icon: "information"
+            },
+            {
+                name: "The Music",
+                path: "/music",
+                icon: "music-note"
+            },
+            {
+                name: "Contact",
+                path: "/contact",
+                icon: "phone"
+            }
+        ]
+    })
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css?family=Montserrat:300,400|Roboto+Mono:300,400&display=swap");
+
+html {
+    font-family: "Montserrat", sans-serif;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+h1 {
+    font-family: "Roboto Mono", monospace;
+    font-size: 38pt;
+    font-weight: 300;
+    letter-spacing: 12pt;
+    text-align: center;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
 }
 </style>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav>
+        <v-app-bar app>
+            <v-spacer></v-spacer>
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+        </v-app-bar>
+        <v-navigation-drawer floating right v-model="drawer" app>
+            <v-img src="@/assets/courier-min.jpg"></v-img>
+            <v-list>
+                <v-list-item
+                    v-for="(item, index) in items"
+                    :key="index"
+                    router
+                    :to="item.path"
+                >
+                    <v-list-item-icon>
+                        <v-icon>mdi-{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content> {{ item.name }} </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "NavigationDrawer",
+    props: {
+        items: Array
+    },
+    data: () => ({
+        drawer: false
+    }),
+    methods: {
+        test: function(item: any) {
+            console.log(item);
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped></style>
