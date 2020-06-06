8ab9250a61cf89ba1d945724b9b99d4a77ffcf23
Lasse Herold Krarup <latze95@gmail.com>
2020-06-06 11:44:32 +0200

    Changed lazy loading to opacity instead of display for better SEO

--- components/Section.vue
+++ components/Section.vue
@@ -1,16 +1,11 @@
 <template>
-  <v-col cols="12" :sm="prominent === true ? 11 : 5" class="pa-8 pa-sm-12">
-    <v-lazy
-      v-model="isActive"
-      transition="scroll-y-reverse-transition"
-    >
+  <v-col cols="12" :sm="prominent === true ? 11 : 5" class="pa-8 pa-sm-12 intersect-wrapper" ref="wrapper" :style="isIntersected ? 'opacity: 1' : ''">
       <div :style="alignCenter === true ? 'text-align: center' : ''">
         <h3 v-if="title !== undefined" :class="titleStyle + '--text'">
           {{ title }}
         </h3>
         <slot></slot>
       </div>
-    </v-lazy>
   </v-col>
 </template>
 
@@ -38,12 +33,32 @@ export default Vue.extend({
     }
   },
   data: () => ({
-    isActive: false
-  })
+    isIntersected: false
+  }),
+  mounted () {
+    const options = {
+      threshold: 0
+    }
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio > 0) {
+        // @ts-ignore
+        this.isIntersected = true
+      }
+    }, options)
+
+    // @ts-ignore
+    observer.observe(this.$refs.wrapper)
+  }
 });
 </script>
 
 <style lang="scss" scoped>
+.intersect-wrapper {
+  transition: opacity ease-in 0.5s;
+  transition-delay: 0.2s;
+  opacity: 0;
+}
+
 h3 {
   font-weight: 500;
   font-size: 1.8rem;
